aa798fa15049acc551a9eca9c5da192d626e8570
Sejal Jain <sejal6249@gmail.com>
2026-06-25 15:22:52 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react' 
+
+const App = () => {
+  return (
+    <>
+    <div id="Dad">
+      <h1 id="child1">Hello</h1>
+      <p id="child2">Welcome to my React application!</p>
+    </div>
+    <div id="Chacha">
+      <h2 id="child3">Another Section</h2>
+      <p id="child4">This is another part of the application.</p>
+    </div>
+    </>
+    
+  )
+}
+
+export default App
